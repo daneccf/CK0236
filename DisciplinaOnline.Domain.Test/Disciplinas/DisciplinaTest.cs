de20871c2235d8a05ec6fea41b9ff408c7c68056
daneccf <danielecavalcantecf@gmail.com>
2020-12-21 22:17:13 -0300

    (vermelho) add teste criar disciplina

---
+++ DisciplinaOnline.Domain.Test/Disciplinas/DisciplinaTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DisciplinaOnline.Domain.Test.Disciplinas
+{
+    public class DisciplinaTest
+    {
+        [Fact]
+        public void CriarDisciplina() // curso-criarcurso-retornacurso
+{
+            var DisciplinaEsperada = new {
+                Nome = "Técnicas de Programação 2",
+                CargaHoraria = (double)64,
+                PublicoAlvo = "Alunos da UFC"
+            };
+
+            var disciplina = new Disciplina(DisciplinaEsperada.Nome,
+                                            DisciplinaEsperada.CargaHoraria,
+                                            DisciplinaEsperada.PublicoAlvo);
+
+            Assert.Equal(DisciplinaEsperada.Nome, disciplina.Nome);
+        }
+    }
+}
